aadc08b1fe8be38698e96eaa4b040f052c4d670e
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-08 09:50:24 +0700

    xong

---
+++ a.js
@@ -0,0 +1,7 @@
+let a = 5;
+let b = '5';
+
+// console.log(a === b);
+//parseFloat
+console.log(++b); //a = a + 1
+// console.log(a);
---
+++ b.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 6;
+
+// console.log(a++ === b);
+
+// console.log(a < 4 && ++b > 10);
+// console.log(b);
+
+// let nang = true;
+// console.log(!nang);
+// console.log(nang);
+
+a %= 10; //a = a + 10 // a = a % 10
+console.log(a);
