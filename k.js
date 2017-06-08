54192e05bd09c1c5bef9db10fd0fbb2addee5e84
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-08 11:18:20 +0700

    rows

---
+++ k.js
@@ -0,0 +1,15 @@
+let dong = 9;
+let str = '';
+let i = 1;
+// for (let i = 1; i <= dong; i++) {
+//     let str = '';
+//     for (let j = 1; j <= i; j++) {
+//         str += j;
+//     }
+//     console.log(str);
+// }
+while(i <= dong) {
+    str += i;
+    console.log(str);
+    i++;
+}
