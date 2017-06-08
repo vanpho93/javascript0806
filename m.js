3fae9cd4a7504275f3faf4dc329ed836893a8d7f
Van Pho Nguyen <vanpho02@gmail.com>
2017-06-08 11:56:24 +0700

    kt

--- m.js
+++ m.js
@@ -5,11 +5,8 @@ for (let i = 1; i <= dong; i++) {
     let str = '';
     let num = 0;
     for (let j = 0; j <= cot; j++) {
-        if (Math.abs(j - dong) < i) {
-            str += ++num;
-        } else {
-            str += ' ';
-        };
+        const aaa = Math.abs(j - dong) < i ? ++num : ' ';
+        str += aaa;
     }
     console.log(str);
 }
